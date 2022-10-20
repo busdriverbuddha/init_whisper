342a4d7e0635906c7741904322cfbdd5159773e1
Guilherme Gama <guilhermegama@gmail.com>
2022-10-20 12:40:09 -0300

    Update init_whisper.sh

--- init_whisper.sh
+++ init_whisper.sh
@@ -1,4 +1,4 @@
-pip3 install git+https://github.com/openai/whisper.git 
+pip3 install git+https://github.com/openai/whisper.git
 pip3 uninstall torch
 pip3 cache purge
 pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
